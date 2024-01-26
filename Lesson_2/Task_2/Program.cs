dbccb9bbbca4a02cd6e1d6a7363007eb4c7dd10b
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-01-26 14:40:11 +0300

    Lesson_2:Task_2

---
+++ Lesson_2/Task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int N = 6;
+int i = 0;
+int[] arr = new int [N];
+
+while (i < N)
+{
+    arr[i] = i + 1;
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
+Console.WriteLine(' ');
+
+
+
+
+int n = 10;
+int[] arr2 = new int[n];
+int j = 0;
+
+while(j < n)
+{
+    arr2[j] = j + 1;
+    Console.Write(arr2[j]);
+    Console.Write(' ');
+    j = j + 1;
+}
+Console.WriteLine(' ');
+
+
+
+
+
+
+
