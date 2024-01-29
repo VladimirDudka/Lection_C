fa6562611aa2466d7346885b9b0a1c99080a1047
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-01-29 13:33:43 +0300

    add more info all Tasks

--- Seminar_2/Task_1/Program.cs
+++ Seminar_2/Task_1/Program.cs
@@ -1,7 +1,16 @@
-﻿// Примеры
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+
+// Примеры
 // а = 256 => 26
 // b = 891 => 81
 
 
-int number = 947;
+int number = 453;
 int firstDigit = number / 100; // 453 / 100 = 4
+int thirdDigit = number % 10; // 453 % 10 = 3
+int result = firstDigit * 10 + thirdDigit; // 4 * 10 + 3 = 43
+// $"Текст {result} текст {a+100} текст {a - 500}"
+Console.WriteLine($"В числе {number} => {result}");
+
--- Seminar_2/Task_2/Program.cs
+++ Seminar_2/Task_2/Program.cs
@@ -1,9 +1,15 @@
-﻿
+﻿// Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
 
 
-int number = 122;
-int secondDigit = number / 10 % 10; // 947 / 10 % 10 = 94 % 10 = 4
-int thirdDigit = number % 10; // 947 % 10 = 7
+int number = 196;
+int secondDigit = number / 10 % 10; // 196 / 10 % 10 = 19 % 10 = 9
+int thirdDigit = number % 10; // 196 % 10 = 6
 // Math.Pow(5, 2) => 5 ^ 2 = 5 * 5 = 25
 int result = (int)Math.Pow(secondDigit, thirdDigit);
 
