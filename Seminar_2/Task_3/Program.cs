873f9e93f7490b898b7263acc23c9c08c6f93c36
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-01-29 13:01:20 +0300

    add Seminar_2:Task_3

---
+++ Seminar_2/Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите
+
+
+int number = 925;
+if (number >= 100)
+{
+    int thirdDigitFromEnd = (number / 100) % 10;
+    Console.WriteLine("Третьей цифры нет.")
+}
+
+else
+{
+    Console.WriteLine("Третьей цифры нет."); 
+
+}
