f2af0cea5ceb3a559004e43e35817119e2b71ba1
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-01-29 12:47:40 +0300

    add Seminar_2:Task_1

---
+++ Seminar_2/Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+int number = 122;
+int secondDigit = number / 10 % 10; // 947 / 10 % 10 = 94 % 10 = 4
+int thirdDigit = number % 10; // 947 % 10 = 7
+// Math.Pow(5, 2) => 5 ^ 2 = 5 * 5 = 25
+int result = (int)Math.Pow(secondDigit, thirdDigit);
+
+Console.WriteLine($"{secondDigit} ^ {thirdDigit} = {result}");
+
