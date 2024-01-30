dbaa31e15a60661bce0f6f217f6ba01dd190f120
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-01-30 14:36:07 +0300

    Simpl_Algorithms:Exercise_3

---
+++ Home_Work/Simpl_ Algorithms/Exercise_3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 3
+// Программа принимает целое число из отрезка [10, 99]
+// и показывает наибольшую цифру числа Пример: 72 => 7, 40 => 4
+
+int value = 45;
+
+if (value >= 10 && value <= 99)  // По условию число из отрезка [10, 99]
+{
+    int first = value / 10; // Первый знак в цифре
+    int second = value % 10; // Второй знак в цифре
+
+    if (first > second)
+    {
+        Console.WriteLine($"В числе {value} наибольшая цифра {first}.");
+    }
+    if (first < second)
+    {
+        Console.WriteLine($"В числе {value} наибольшая цифра {second}.");
+    }
+    if (first == second)
+    {
+        Console.WriteLine($"В числе {value} цифра {first} и цифра {second} равны.");
+    }
+
+}
+else
+{
+    Console.WriteLine($"По условию число должно находиться в промежутке от 10 до 99.");
+}
