08ae87f297de863ed78dd1fd40321a0e59b8c860
Wayfarer <wayfarer.init79@gmail.com>
2024-02-03 00:17:21 +0300

    Lesson_2:Task_5

---
+++ Lesson_2/Task_5/Program.cs
@@ -0,0 +1,26 @@
+﻿// Цикл For ...
+// В цикле for в теле цикла задаётся,
+// перемен. счётчик, условие, и изменение условия
+// for (int i = 0; i < 10; i++)
+//
+
+
+// Проход по элементам массива 
+int[] array = {2,4,6,8,10};
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}, ");
+}
+Console.WriteLine($"End array.");
+
+
+// Задача, где число итераций фиксировано
+int n = 100;
+int sum = 0;
+for (int i = 1; i <= n; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine(sum);
+
+
