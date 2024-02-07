c19dff1a86bfcb8d7b37d9f6b9611286eb37e2a0
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-02-07 14:36:40 +0300

    modern Lesson_3:Task_1

--- Lesson_3/Task_1/Program.cs
+++ Lesson_3/Task_1/Program.cs
@@ -11,15 +11,33 @@
 double CalculateFormula(int a, double b, int c, double d)
 {
     double numenator = a * b;
-    double denumenator = c + b;
+    double denumenator = c + d;
     double result = numenator / denumenator;
     return result;
 }
 
 //CalculateFormula(3,-3,6,-6);
-double total = CalculateFormula(7,-3,1,-6);
+//double total = CalculateFormula(f,1,1,-6);
 Console.Clear();
-Console.WriteLine(total);
+Console.WriteLine($"Введите первое значение (a): ");
+int f = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Введите второе значение (b): ");
+double g = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Введите третье значение (c): ");
+int h = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Введите четвертое значение (d): ");
+double j = double.Parse(Console.ReadLine()!);
+
+double total = CalculateFormula(f,g,h,j);
+
+Console.Clear();
+Console.WriteLine($"Метод CalculateFormula: ({f} * {g}) / ({h} + {j}) = {total}.");
+Console.WriteLine();
+Console.WriteLine($"Результат вычисления метода \"CalculateFormula\" равен {total}.");
+Console.WriteLine();
 
 
 
