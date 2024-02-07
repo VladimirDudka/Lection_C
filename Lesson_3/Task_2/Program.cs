6307a7f45ce147d1d75b43f69d6b80697b197287
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-02-07 15:24:57 +0300

    Square_number Lesson_3:Task_2

--- Lesson_3/Task_2/Program.cs
+++ Lesson_3/Task_2/Program.cs
@@ -1,12 +1,25 @@
-﻿// 
-//
-//
-//
-//
-//
+﻿// Задача 2
+// Вывод на экран квадратов чисел от 1 до N
+
+
+// Создаём Метод Square_number()
+void Square_number(int lim)
+{
+    int i = 1;
+    while(i <= lim)
+    {
+        Console.WriteLine($"Квадрат числа {i} равен {i * i}.");
+        i++;
+    }
+}
 
 
 
+// Вызов метода Square_number(), в метод передаём
+// переменную lim (до какого числа, узнать квадрат этого числа)
+Console.Clear();
+Square_number(15); // Здесь число можно менять
+Console.WriteLine();
 
 
 
