760eba97732502a2c10977ef8ec32e2ae0517f72
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-02-08 10:18:51 +0300

    Lesson_3:Task_4

---
+++ Lesson_3/Task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿//  Задача 3
+//
+//  Метод который обнуляет четные числа массива
+
+void ZeroEventElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+
+void PrintArray(int[] arr)
+{
+    for (int e = 0; e < arr.Length; e++ )
+    {
+        Console.Write($" {arr[e]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = {5, 3, 9, 7, 1};
+ZeroEventElements(array);
+PrintArray(array);
+
+
+
+
+
+
---
+++ Lesson_3/Task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿//                Генерация случайных чисел.
+//
+//  Random - Тип данных "Генератор случайных чисел"
+//  rnd - Имя переменной
+//  new Random() - создание объекта
+
+Random rnd = new Random(); // Объект типа Random (переменная rnd только ссылается на объект)
+// Заполнение массива
+int size = 256;
+int[] arr_int = new int[size];
+int i = 0;
+while (i < size)
+{
+    arr_int[i] = rnd.Next(-9, 10);
+    i += 1;
+}
+
+// Вывод на экран массива
+i = 0;
+while (i < size)
+{
+    Console.Write($"{arr_int[i]} ");
+    i += 1;
+}
+Console.WriteLine();
+
