b1922e196d12c6d2bb9b6b1a13e6697ed371b0d0
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-02-09 16:01:18 +0300

    Seminar_4:Task_5

---
+++ Seminar_4/Task_5/Program.cs
@@ -0,0 +1,53 @@
+﻿//               В C#,  ФУНКЦИЯ == МЕТОД .
+//
+//          Второй способ решения Задачи Task_4
+//  Заполните массив на N (вводится с консоли, не более 8)
+// случайных целых чисел от 0 до 9.
+// Сформируйте целое число, которое будет состоять из цифр
+// данного массива. Пример: [2, 7, 4] => 274
+
+
+// Метод создаёт массив из указанного кол-ва элементов
+// и заполняет его случайными числами из указанного промежутка
+
+int[] RandArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 10);
+    }
+    return arr;
+}
+
+// Метод склеивает элементы переданного массива в одну строку
+// Пример: [2, 7, 4] => 274
+
+string paste_togetherArray(int[] array)
+{
+    string glue_number = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        glue_number += array[i].ToString();
+    }
+    return glue_number;
+}
+
+// Ввод данных и запуск метода
+Console.Clear();
+Console.Write($"Введите кол-во элементов массива от 1 до 8 : ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// проверка на N (вводится с консоли, не более 8)
+if (N < 1 | N > 8)
+{
+    Console.WriteLine($"Внимательно читай условие!");
+}
+else
+{
+    int[] method = RandArray(N);
+    Console.WriteLine();
+    Console.WriteLine($" Массив : [ {string.Join(" : ", method)} ] .");
+    Console.WriteLine($" Число :  {paste_togetherArray(method)} .");
+    Console.WriteLine();
+}
