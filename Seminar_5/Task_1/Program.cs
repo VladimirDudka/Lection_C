f2acc9e67c64584bb12145df586aa39dcbd7c46d
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-02-14 14:28:16 +0300

    modified Seminar_5:Task_1

--- Seminar_5/Task_1/Program.cs
+++ Seminar_5/Task_1/Program.cs
@@ -4,11 +4,11 @@
 Random rnd = new Random();
 int[,] DoubleArray(int row, int colum) // Метод создания двухмерного масива,
 //  с произвольными целыми числами от 10 до 99
-{   
+{
     int[,] arr = new int[row, colum];
-    for(int i = 0; i < row; i++)
+    for (int i = 0; i < row; i++)
     {
-        for(int j = 0; j < colum; j++)
+        for (int j = 0; j < colum; j++)
         {
             arr[i, j] = rnd.Next(10, 100);
         }
@@ -16,17 +16,15 @@ int[,] DoubleArray(int row, int colum) // Метод создания двухм
     return arr;
 }
 
-
-
 int[,] ChangeElements(int[,] arrForChange) // Метод находит элементы, у которых оба
 //    числа чётные, заменяет эти элементы на их квадраты.
 {
-    for(int i = 0; i < arrForChange.GetLength(0); i++)
+    for (int i = 0; i < arrForChange.GetLength(0); i++)
     {
-        for(int j = 0; j < arrForChange.GetLength(1); j++)
+        for (int j = 0; j < arrForChange.GetLength(1); j++)
         {
             int temp = arrForChange[i, j];
-            if((temp / 10 ) % 2 == 0 && (temp % 10 ) % 2 == 0)
+            if ((temp / 10) % 2 == 0 && (temp % 10) % 2 == 0)
             {
                 temp *= temp;
                 arrForChange[i, j] = temp;
@@ -36,20 +34,18 @@ int[,] ChangeElements(int[,] arrForChange) // Метод находит элем
     return arrForChange;
 }
 
-
-void SeeMatrix(int[,] arr)  // Метод печатающий двумерный массив
+void SeeMatrix(int[,] arr) // Метод печатающий двумерный массив
 {
-    for(int i = 0; i < arr.GetLength(0); i++)
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
         Console.Write("|");
-        for(int j = 0; j < arr.GetLength(1); j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
             Console.Write($"  {arr[i, j]}");
         }
         Console.WriteLine("  |");
     }
 }
-
 
 Console.Clear();
 
@@ -59,7 +55,7 @@ int row = Convert.ToInt32(Console.ReadLine()!);
 Console.Write("Введите кол-во строк массива (не более 5): ");
 int colum = Convert.ToInt32(Console.ReadLine()!);
 
-int [,] forPrint = DoubleArray(row, colum);
+int[,] forPrint = DoubleArray(row, colum);
 
 Console.WriteLine($"Полученный массив : ");
 SeeMatrix(forPrint);
@@ -68,4 +64,3 @@ Console.WriteLine();
 Console.WriteLine($"Изменёный массив : ");
 SeeMatrix(ChangeElements(forPrint));
 Console.WriteLine();
-
