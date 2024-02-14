bd3d579044971c48473e97cbac050b38c8263c3a
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-02-14 13:27:11 +0300

    Seminar_5:Task_1

---
+++ Seminar_5/Task_1/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба
+// числа чётные, замените эти элементы на их квадраты.
+
+Random rnd = new Random();
+int[,] DoubleArray(int row, int colum)
+{   
+    int[,] arr = new int[row, colum];
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < colum; j++)
+        {
+            arr[i, j] = rnd.Next(10, 100);
+        }
+    }
+    return arr;
+}
+
+
+
+int[,] ChangeElements(int[,] arrForChange)
+{
+    for(int i = 0; i < arrForChange.GetLength(0); i++)
+    {
+        for(int j = 0; j < arrForChange.GetLength(1); j++)
+        {
+            int temp = arrForChange[i, j];
+            if((temp / 10 ) % 2 == 0 && (temp % 10 ) % 2 == 0)
+            {
+                temp *= temp;
+                arrForChange[i, j] = temp;
+            }
+        }
+    }
+    return arrForChange;
+}
+
+
+void SeeMatrix(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"  {arr[i, j]}");
+        }
+        Console.WriteLine("  |");
+    }
+}
+
+
+Console.Clear();
+
+Console.Write("Введите кол-во столбцов массива (не более 5): ");
+int row = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введите кол-во строк массива (не более 5): ");
+int colum = Convert.ToInt32(Console.ReadLine()!);
+
+int [,] forPrint = DoubleArray(row, colum);
+Console.WriteLine($"Полученный массив : ");
+SeeMatrix(forPrint);
+Console.WriteLine();
+Console.WriteLine($"Изменёный массив : ");
+SeeMatrix(ChangeElements(forPrint));
+Console.WriteLine();
+
