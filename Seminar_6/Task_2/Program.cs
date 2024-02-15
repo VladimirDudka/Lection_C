91ed06cdcd04df1af38c41f29593c66cbed7c3c7
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-02-15 15:15:04 +0300

    Seminar_6:Task_2

---
+++ Seminar_6/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//   На основе символов строки (тип string)
+//  сформировать массив символов.
+//  Вывести массив на экран.
+// "Hello!" = ['H', 'e', 'l', 'l', 'o', '!']
+
+char[] InStringOutChar(string text)
+{
+    char[] symbols = new char[text.Length];
+    for (int i = 0; i < text.Length; i++)
+    {
+        symbols[i] = text[i];
+    }
+    return symbols;
+}
+Console.Clear();
+string str = "Hello world!";
+Console.WriteLine($"Строка входа - {str}");
+char[] chars = InStringOutChar(str);
+Console.WriteLine($"Полученный массив - [ {string.Join(" _ ", chars)}]");
