82f5531450b2e6979f2418e30944e3a78c92ad6b
Vladimir Dudka <wayfarer.init79@gmail.com>
2024-02-16 13:54:19 +0300

    Seminar_6:Home_Work_2

---
+++ Seminar_6/Home_Work_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//    Задача 2: 
+// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string StringToLower(string text)
+{
+    string lower = text;
+    lower = lower.ToLower();
+    return lower;
+}
+
+Console.Clear();
+Console.Write("Напишите текст на \"латинице\" с разным регистром : ");
+string forPrint = Console.ReadLine()!;
+Console.WriteLine();
+Console.WriteLine($"Введённый текст : {forPrint}");
+Console.WriteLine($"Полученный текст : {StringToLower(forPrint)}");
+Console.WriteLine();
