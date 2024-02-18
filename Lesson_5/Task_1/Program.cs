13cf39efbddcc7a8cffde5f4f0900ece8252ae35
Wayfarer <wayfarer.init79@gmail.com>
2024-02-18 22:03:35 +0300

    config commit

---
+++ Lesson_5/Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//                                РЕКУРСИЯ
+//         Задача_1
+//  Вычислить факториал от натурального числа N
+int FindFactorial(int N)
+{
+    // Базовый случай
+    if(N == 1)
+    {
+        return 1;
+    }
+
+    // Рекурсивный случай
+    return N * FindFactorial(N - 1);
+}
+
+
+Console.WriteLine(FindFactorial(5));
