272f6e529387354e289aed2a6eaa5cf21a21404f
Wayfarer <wayfarer.init79@gmail.com>
2024-02-18 23:30:35 +0300

    Lesson_5:Task_1 + add home work

--- Lesson_5/Task_1/Program.cs
+++ Lesson_5/Task_1/Program.cs
@@ -3,15 +3,19 @@
 //  Вычислить факториал от натурального числа N
 int FindFactorial(int N)
 {
+
     // Базовый случай
-    if(N == 0)
+    if (N == 1 || N == 0)
     {
+        Console.WriteLine($"Stop reqursion : N = {N}");
         return 1;
     }
 
     // Рекурсивный случай
-    return N * FindFactorial(N - 1);
+    Console.WriteLine(N);
+    int res = N * FindFactorial(N - 1);
+    Console.WriteLine($"Возврат: N = {N}, Factorial = {res}");
+    return res;
 }
-
-
-Console.WriteLine(FindFactorial(6));
+int simplyDigit = 5;
+Console.WriteLine($"Факториал числа {simplyDigit} равен {FindFactorial(simplyDigit)}");
