e3c4e32b34fac789c2653bbab06b14f9cb823e16
Wayfarer <wayfarer.init79@gmail.com>
2024-02-19 16:23:18 +0300

    Seminar_7:Home_work_1

---
+++ Seminar_7/Home_work_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N 
+// Использовать рекурсию не использовать циклы
+void ShowNumbers(int M, int N)
+{
+    // Базовый случай
+    if(M == N)
+    {
+        //Console.Clear();
+        Console.Write($"{M} ");
+        Console.WriteLine("END.");
+        return;
+    }
+    // Рекурсивный случай
+    Console.Write($"{M} ");
+    ShowNumbers(M + 1, N);
+
+    // Раскручивание рекурсии
+    Console.Write($"{M} ");
+}
+
+// Для проверки в метод ShowNumbers(), нужно добавить входн. параметры
+ShowNumbers(5,9);
