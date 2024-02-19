119a06288880ac7292ec2f9282554221b2c98f56
Wayfarer <wayfarer.init79@gmail.com>
2024-02-19 15:54:39 +0300

    Seminar_7:Task_3

---
+++ Seminar_7/Task_3/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Задайте значение N. 
+//  Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+//Пример : 
+// N = 5 => 1 2 3 4 5
+// start = 1, end = N
+
+void ShowNumbers(int start, int end)
+{
+    // Базовый случай
+    if(start == end)
+    {
+        //Console.Clear();
+        Console.Write($"{start} ");
+        Console.WriteLine("END.");
+        return;
+    }
+    // Рекурсивный случай
+    Console.Write($"{start} ");
+    ShowNumbers(start + 1, end);
+
+    // Раскручивание рекурсии
+    Console.Write($"{start} ");
+}
+
+ShowNumbers(5,17);
